4f630720d9a75f390d43e4f3fd6c684d11da9c09
Maha <maha053222@gmail.com>
2023-09-03 03:06:56 +0300

    Task 2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Sextant</h1>
-      </div>
-      <div>
-        components
-      </div>
+        <Banner bannerText="Sextant" />
+        <Exhibit name="Exhibit 1"></Exhibit>
+        <Exhibit name="Exhibit 2"></Exhibit>
+        <Exhibit name="Exhibit 3"></Exhibit>
     </div>
   );
 }
